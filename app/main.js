fec2c8dff6efe9d7cad4e7d16b40306f9ca40399
mandilinville <mandi.linville@gmail.com>
2017-01-31 17:04:12 -0700

    Initial commit

--- app/main.js
+++ app/main.js
@@ -1,44 +1,50 @@
-(function(){
+(function () {
 
-  var fs = new FlixService()  
+  var fs = new FlixService()
 
 
   new Vue({
     el: '#vue-flix',
-    data: function(){
+    data: function () {
       return {
         category: 'actor',
         query: '',
         movies: [],
-        details: {}
+        details: {},
+        myWatchlist: []
       }
     },
-    methods:{
-      search: function(){
-        if(this.category == 'title'){
+    methods: {
+      search: function () {
+        if (this.category == 'title') {
           fs.getFlix(this.category, this.query, this.setDetails)
           return
         }
         fs.getFlix(this.category, this.query, this.setMovies)
       },
-      setMovies: function(movies){
+      setMovies: function (movies) {
         this.movies = movies
       },
-      reset: function(){
+      reset: function () {
         this.query = ''
-        this.category = '',
+        this.category = ''
         this.movies = []
+        this.details = {}
+        
       },
-      setDetails: function(movie){
+      setDetails: function (movie) {
         this.details = movie
       },
-      clearDetails: function(){
+      clearDetails: function () {
         this.details = {}
+      },
+      addToMyWatchlist: function (movie) {
+        this.myWatchlist.push(movie)
+      },
+      removeFromMyWatchlist: function (movie) {
+        var i = this.myWatchlist.indexOf(movie)
+        this.myWatchlist.splice(i, 1)
       }
     }
   })
-
-  
-
-
 }())
